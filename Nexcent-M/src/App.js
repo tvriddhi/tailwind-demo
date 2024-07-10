372ff2be5c6188644a77ce70e6165093d0d88a46
tvmeetb <meet.b@techvoot.com>
2024-07-10 19:06:49 +0530

    banner and our client sec add

--- Nexcent-M/src/App.js
+++ Nexcent-M/src/App.js
@@ -1,7 +1,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import './App.css';
-import { logo } from './assets/images';
+import { heroImg, logo, Logo1, Logo2, Logo3, Logo4, Logo5, manageIcon } from './assets/images';
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -17,81 +17,132 @@ function classNames(...classes) {
 }
 function App() {
   return (
-    <div className="xl:container lg:container px-6 sm:px-14 lg:px-6 lg:py-7 py-4">
-      <Disclosure as="nav">
-      <div>
-        <div className="relative flex items-center justify-between">
-          <div className="flex flex-1 items-center lg:justify-start">
-            <div>
-              <img
-                alt="Your Company"
-                src={logo}
-              />
-            </div>
-            <div className="hidden lg:block mx-auto">
-              <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
-                    className={classNames(
-                      item.current ? 'text-blue-950 font-medium' : 'text-blue-950 font-normal',
-                      'rounded-md px-3 py-2',
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
+    <>
+      <div className="xl:container lg:container px-6 sm:px-14 lg:px-6 lg:py-7 py-4">
+        <Disclosure as="nav">
+        <div>
+          <div className="relative flex items-center justify-between">
+            <div className="flex flex-1 items-center lg:justify-start">
+              <div>
+                <img
+                  alt="Your Company"
+                  src={logo}
+                />
+              </div>
+              <div className="hidden lg:block mx-auto">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      aria-current={item.current ? 'page' : undefined}
+                      className={classNames(
+                        item.current ? 'text-blue-950 font-medium' : 'text-blue-950 font-normal',
+                        'rounded-md px-3 py-2',
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
               </div>
             </div>
+            <div className="space-x-4 hidden lg:flex">
+              <a href="#" className='text-green-600 rounded-md px-5 py-3 text-sm font-medium hover:bg-green-200/50 transition duration-500 ease-out'>
+                Login
+              </a>
+              <a href="#" className='text-white bg-green-600 rounded-md px-5 py-3 text-sm font-medium'>
+                Sign up
+              </a>
+            </div>
+            <div className="lg:hidden">
+              {/* Mobile menu button*/}
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span className="-inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+                <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+              </DisclosureButton>
+            </div>
           </div>
-          <div className="space-x-4 hidden lg:flex">
-            <a href="#" className='text-green-600 rounded-md px-5 py-3 text-sm font-medium hover:bg-green-200/50 transition duration-500 ease-out'>
+        </div>
+
+        <DisclosurePanel className="lg:hidden">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+              <DisclosureButton
+                key={item.name}
+                as="a"
+                href={item.href}
+                aria-current={item.current ? 'page' : undefined}
+                className={classNames(
+                  item.current ? 'text-blue-950' : 'text-blue-950 hover:bg-gray-700 hover:text-white',
+                  'block rounded-md px-3 py-2 text-base font-medium',
+                )}
+              >
+                {item.name}
+              </DisclosureButton>
+            ))}
+            <a href="#" className='text-green-600 inline-block rounded-md px-3 py-2 text-sm font-medium hover:bg-green-200/50'>
               Login
             </a>
-            <a href="#" className='text-white bg-green-600 rounded-md px-5 py-3 text-sm font-medium'>
+            <a href="#" className='text-white bg-green-600 inline-block rounded-md px-3 ml-3 py-2 text-sm font-medium'>
               Sign up
             </a>
           </div>
-          <div className="lg:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="-inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
-            </DisclosureButton>
-          </div>
-        </div>
+        </DisclosurePanel>
+      </Disclosure>
       </div>
 
-      <DisclosurePanel className="lg:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={classNames(
-                item.current ? 'text-blue-950' : 'text-blue-950 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-          <a href="#" className='text-green-600 inline-block rounded-md px-3 py-2 text-sm font-medium hover:bg-green-200/50'>
-            Login
-          </a>
-          <a href="#" className='text-white bg-green-600 inline-block rounded-md px-3 ml-3 py-2 text-sm font-medium'>
-            Sign up
-          </a>
+      <section className='py-24 bg-slate-50'>
+        <div className='xl:container lg:container px-6 sm:px-14 lg:px-6'>
+          <div className='grid grid-cols-12 gap-4 items-center'>
+            <div className='lg:col-span-7 col-span-12'>
+              <h1 className='text-6xl'>Lessons and insights <span className='text-green-600'>from 8 years</span></h1>
+              <p className='mt-4 text-gray-600'>Where to grow your business as a photographer: site or social media?</p>
+              <button className='btn-primary py-3 px-8 rounded-sm mt-8'>Register</button>
+            </div>
+              <div className='lg:col-span-4 lg:col-start-9 col-start-3 col-span-8'>
+                <img
+                  className='w-full'
+                  alt="hero img"
+                  src={heroImg}
+                  />
+              </div>
+          </div>
         </div>
-      </DisclosurePanel>
-    </Disclosure>
-    </div>
+      </section>
+      <section className='our-client py-10'>
+          <div className='xl:container lg:container px-6 sm:px-14 lg:px-6'>
+            <h2 className='text-4xl text-gray-950 font-semibold text-center mb-2'>Our Clients</h2>
+            <p className='text-gray-600 text-center mb-4'>We have been working with some Fortune 500+ clients</p>
+            <div className='flex content-between gap-4 flex-wrap'>
+              <img alt="Your Company" src={Logo1}/>
+              <img alt="Your Company" src={Logo2}/>
+              <img alt="Your Company" src={Logo3}/>
+              <img alt="Your Company" src={Logo4}/>
+              <img alt="Your Company" src={Logo5}/>
+            </div>
+          </div>
+      </section>
+      <section>
+        <div className='xl:container lg:container px-6 sm:px-14 lg:px-6'>
+          <h2 className='text-4xl text-gray-950 font-semibold text-center max-w-[542px] mx-auto mb-2'>Manage your entire community in a single system</h2>
+          <p className='text-gray-600 text-center mb-4'>We have been working with some Fortune 500+ clients</p>
+          <div className='grid grid-cols-12 gap-4'>
+            <div className='col-span-3 col-start-1'>
+              <img alt='manageIcon' src={manageIcon}/>
+            </div>
+            <div className='col-span-3 col-start-1'>
+              <img alt='manageIcon' src={manageIcon}/>
+            </div>
+            <div className='col-span-3 col-start-1'>
+              <img alt='manageIcon' src={manageIcon}/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
--- Nexcent-M/tailwind.config.js
+++ Nexcent-M/tailwind.config.js
@@ -19,11 +19,24 @@ module.exports = {
         green: {
           600:'#4CAF4F',
           200:'#bbf7d0',
+        },
+        gray: {
+          600: "#717171",
+          950:"#4D4D4D",
+        },
+        slate: {
+          50:"#F5F7FA"
         }
+      },
+      fontSize: {
+        '6xl': ['4rem', '1.2'], // 64px
       },
       container: {
         center: true,
       },
+      borderRadius: {
+      'sm': '0.25rem',
+    }
     },
   },
   plugins: [],
